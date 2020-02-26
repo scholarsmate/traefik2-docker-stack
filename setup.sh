edaa85635f1a09fc9e59c8c3538350e7740d99d5
Davin Shearer <davin.shearer@evc.ctc.com>
2020-02-26 18:00:13 -0500

    Define service domain as an environment variable

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+SVC_DOMAIN=${SVC_DOMAIN:-domain.com}
+
+echo '##########################################################################'
+echo '#               About to run setup.sh script                             #'
+echo '##########################################################################'
+echo "SVC_DOMAIN=${SVC_DOMAIN}"
+
 set -ex
 
 docker network create --driver overlay traefik-proxy >/dev/null 2>&1 || true
 for app in traefik2 metrics portainer dbadmin atlassian gitlab nexus rocketchat; do
-   docker stack deploy --compose-file $app/docker-compose.yml devops
+   SVC_DOMAIN="${SVC_DOMAIN}" docker stack deploy --compose-file $app/docker-compose.yml devops
 done
